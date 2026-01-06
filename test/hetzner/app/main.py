eb05707fb19f5e274cb7e63a092bf400d9ae3cbc
Justin Joseph <josephmjustin@gmail.com>
2026-01-06 20:12:42 +0000

    api auth test

--- test/hetzner/app/main.py
+++ test/hetzner/app/main.py
@@ -7,11 +7,11 @@ load_dotenv()  # load API_TOKEN from .env
 app = FastAPI()
 API_TOKEN = os.getenv("API_TOKEN")
 
-def verify_token(x_token: str = Header(...)):
-    if x_token != API_TOKEN:
-        raise HTTPException(status_code=401, detail="Unauthorized")
+def verify_token(x_token: str = Header(None)):
+    if not x_token or x_token != API_TOKEN:
+        raise HTTPException(status_code=401, detail="Unauthorized access")
 
 @app.get("/data")
-def get_data(token: str = Header(...)):
+def get_data(token: str = Header(None)):
     verify_token(token)
     return {"message": "Authorized access"}
